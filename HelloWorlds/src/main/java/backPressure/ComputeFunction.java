b44616b4ac184c8f08464844a383e46bd07f93fa
Gordo851 <gr2@hw.ac.uk>
2019-02-28 18:00:47 +0000

    yep

---
+++ HelloWorlds/src/main/java/backPressure/ComputeFunction.java
@@ -0,0 +1,21 @@
+package backPressure;
+
+import io.reactivex.Observable;
+
+/*use sleeps to make an observable produce every half a second and an observable that 
+ * consumes every second
+ * 
+ */
+public class ComputeFunction {
+	public void creatingData() {
+	Observable<String> observable = Observable.create(s -> {
+		s.onNext("Hello");
+		s.onNext("World");
+	});
+	
+	observable
+	.filter(thisString -> !thisString.equals("World"))
+	.map(thisString -> thisString + " world")
+	.subscribe(System.out::println);
+	}
+}
